2aab9a25a2744815537431c94f5ddaa1a61c6591
aficat <aficatrashid@gmail.com>
2020-05-07 17:06:16 +0800

    Updates

---
+++ precache-manifest.e7cf3ef87885ad673d27bafa6a409d3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04cae9eb08a69420dac84114e5ddcb8c",
+    "url": "/galleria/index.html"
+  },
+  {
+    "revision": "82081f42e61ebf9c5d7e",
+    "url": "/galleria/static/css/main.a2887fda.chunk.css"
+  },
+  {
+    "revision": "d8979e8e95ba02f88a68",
+    "url": "/galleria/static/js/2.0d409ba3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/galleria/static/js/2.0d409ba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82081f42e61ebf9c5d7e",
+    "url": "/galleria/static/js/main.32891f38.chunk.js"
+  },
+  {
+    "revision": "9bd0700eae5e0722697f",
+    "url": "/galleria/static/js/runtime-main.57f37e5d.js"
+  }
+]);
